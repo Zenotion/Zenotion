ef530e77793b53063e6dcef43a6b45ef21632748
Nivaas B <cocnivaas@gmail.com>
2024-12-30 11:59:54 +0530

    testing

--- testing/testing.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('toggle-sidebar').addEventListener('click', function() {
-  var sidebar = document.getElementById('sidebar');
-  var content = document.querySelector('.content-bar');
-
-  sidebar.classList.toggle('hidden');
-  content.classList.toggle('full-width');
-});
